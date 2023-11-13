8504920cb69609b6b225296907baac17a4bfe561
Valwolfor <diegofelipe.gallo@gmail.com>
2023-11-12 23:21:08 -0500

    Primer commit

---
+++ src/main/java/com/magicqoven/MagicQovenApplication.java
@@ -0,0 +1,13 @@
+package com.magicqoven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicQovenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicQovenApplication.class, args);
+    }
+
+}
